f9a0076397ff115cff1d296bec82e4a9af4cf8a2
root <fenugreo@in-c1.nsk.21-school.ru>
2024-11-02 15:47:30 +0700

    REWORK DONE

---
+++ БДПользователя/scripts/users_structure.sh
@@ -0,0 +1,24 @@
+mkdir -p ../users/
+
+read -p "Введите ник: " nickname
+
+mkdir -p ../users/$nickname/photos
+echo "Папка photos сделана"
+mkdir -p ../users/$nickname/photos/202{0..4}
+echo "Папки с 2020 по 2024 созданы"
+touch ../users/$nickname/photos/202{0..4}/default.png
+echo "default png cозданы"
+
+mkdir -p ../users/$nickname/videos
+echo "Папка videos сделана"
+mkdir -p ../users/$nickname/videos/202{0..4}
+echo "Папки с 2020 по 2024 созданы"
+touch ../users/$nickname/videos/202{0..4}/default.mp4
+echo "default mp4 cозданы"
+
+mkdir -p ../users/$nickname/documents
+echo "Папка documents сделана"
+mkdir -p ../users/$nickname/documents/202{0..4}
+echo "Папки с 2020 по 2024 созданы"
+touch ../users/$nickname/documents/202{0..4}/default.mp4
+echo "default mp4 cозданы"
